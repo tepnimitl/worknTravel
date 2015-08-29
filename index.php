2e04d07bfecadb899b33e7f94739495eedb0d057
Tepnimit <tepnimitl@gmail.com>
2015-08-29 03:41:12 +0000

    Update Content and Layout

--- index.php
+++ index.php
@@ -11,8 +11,8 @@
 			NREUMQ.push(["mark", "firstbyte", new Date().getTime()]);</script>-->
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+		<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>-->
+		<!--<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>-->
 		<link href="css/style.css" rel="stylesheet" type="text/css">
 		<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
 		
